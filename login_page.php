b8e002854909d6edca47feb3c5ae9c4a136d380b
Sarah Hanis <ai220031@student.uthm.edu.my>
2024-06-03 15:07:13 +0800

    Innitial commit with PHP and CSS files

--- login_page.php
+++ login_page.php
@@ -6,15 +6,17 @@
 </head>
 <body>
     <div class="container">
-        <h2>Login</h2>
-        <form action="login.php" method="post">
-            <label for="matric">Matric:</label>
-            <input type="text" id="matric" name="matric" required><br><br>
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password" required><br><br>
-            <input type="submit" value="Login">
-        </form>
-        <p>Register <a href="registration_page.php">here</a> if you have not.</p>
+        <div class="form-wrapper">
+            <h2>Login</h2>
+            <form action="login.php" method="post">
+                <label for="matric">Matric:</label>
+                <input type="text" id="matric" name="matric" required>
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" required>
+                <input type="submit" value="Login">
+            </form>
+            <p>Register <a href="registration_page.php">here</a> if you have not.</p>
+        </div>
     </div>
 </body>
 </html>
